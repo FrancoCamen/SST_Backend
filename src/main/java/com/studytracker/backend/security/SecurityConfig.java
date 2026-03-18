016a383312b7c3d60809f7a7f094d5e9aa4418d4
FrancoCamen <francocamen1@gmail.com>
2026-03-18 03:14:53 -0300

    fix: se agrego un filtro .headers

--- src/main/java/com/studytracker/backend/security/SecurityConfig.java
+++ src/main/java/com/studytracker/backend/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .headers(headers -> headers.frameOptions(frame -> frame.disable()))
             .cors(cors -> cors.configurationSource(corsConfigurationSource))
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
